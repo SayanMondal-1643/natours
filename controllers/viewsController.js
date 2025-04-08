02bd17838e78f18d5b91dcf344d48ba1ad69be59
Sayan Mondal <kausikm76@gmail.com>
2025-04-08 13:00:14 +0530

    Corrected a spelling mistake (≖_≖)

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -9,7 +9,7 @@ exports.alerts = (req, res, next) => {
 
   if (alert === 'booking') {
     res.locals.alert =
-      "Your booking was successful! Please check youe email for a confirmation. If your booking doesn't show up here immediately, please come back later.";
+      "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up here immediately, please come back later.";
   }
 
   next();
